dd8455c082491c4f77a43d61334fe768b39836e8
10907389 <10907389@F207-9>
2014-02-10 09:56:33 +0100

    create class Marin

---
+++ src/Marin.java
@@ -0,0 +1,56 @@
+
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="marin")
+@XmlAccessorType(XmlAccessType.FIELD)
+
+@Entity
+
+public  class Marin  implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+
+
+	@XmlAttribute(name="id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	
+
+	@XmlElement
+	private String nom;
+
+	@XmlElement
+	private String prenom;
+
+
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Long getId() {
+		return id;
+	}
+}
